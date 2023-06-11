feda5e34ce0985aee6c4c838b68ded9ae2110755
joaoand <joao.demata@correo.unimet.edu.ve>
2023-06-11 18:34:16 -0400

    Vistas de my orders, mejoras en my order y nuevos enrutadores

--- src/pages/my_order/index.jsx
+++ src/pages/my_order/index.jsx
@@ -1,10 +1,33 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+import { ShoppingCardContext } from '../../context';
 import Layout from '../../components/layout'
+import OrderCard from '../../components/order_card';
 
 function MyOrder() {
+  const context = useContext(ShoppingCardContext);
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+
+  if(index === 'last') index = context.order?.length - 1 
 
   return (
     <Layout>
-      MyOrder
+      <div className='flex items-center justify-center relative w-80 mb-6'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer"/>
+        </Link>
+        <h1>MyOrder</h1>
+
+      </div>
+          <div className='flex flex-col w-80'>
+                {
+                    context.order?.[index].products.map(_product => {
+                        return <OrderCard key={_product.id} id={_product.id} title={_product.title} imageUrl={_product.images} price={_product.price}/>
+                    })
+                }
+            </div>
     </Layout>
   )
 }
---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+/**
+ * This function calculates total price of a new order
+ * @param {Array} _products - cartProducts Array of objects
+ * @returns {Number} Total price
+ */
+export const totalPrice = (_products) => {
+    let sum = 0
+
+    _products.forEach(_product =>{
+        sum += parseFloat(_product.price)
+    })
+
+    return sum;
+}
